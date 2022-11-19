bb89457c10ede1a538f0e66f57d891d959d15517
ImaneBoutata <imanebt6@gmail.com>
2022-11-19 10:40:08 +0100

    first commit

--- BiblioProject/Form1.Designer.cs
+++ BiblioProject/Form1.Designer.cs
@@ -28,19 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.prenomBox = new System.Windows.Forms.TextBox();
+            this.cinBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(3, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1001, 143);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nom";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(305, 207);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(150, 31);
+            this.nameBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(612, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prénom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(142, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CIN";
+            // 
+            // prenomBox
+            // 
+            this.prenomBox.Location = new System.Drawing.Point(305, 303);
+            this.prenomBox.Name = "prenomBox";
+            this.prenomBox.Size = new System.Drawing.Size(150, 31);
+            this.prenomBox.TabIndex = 6;
+            // 
+            // cinBox
+            // 
+            this.cinBox.Location = new System.Drawing.Point(305, 382);
+            this.cinBox.Name = "cinBox";
+            this.cinBox.Size = new System.Drawing.Size(150, 31);
+            this.cinBox.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1005, 550);
+            this.Controls.Add(this.cinBox);
+            this.Controls.Add(this.prenomBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private TextBox nameBox;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private TextBox prenomBox;
+        private TextBox cinBox;
     }
 }
